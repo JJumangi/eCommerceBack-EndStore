40bbbe5f60d88ff77b17714d39e4584d16ce025c
Jon A <jon.jumangi@gmail.com>
2023-08-11 21:28:55 -0500

    initial

---
+++ Develop/routes/api/category-routes.js
@@ -0,0 +1,40 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', (req, res) => {
+  Category.findAll({
+    include: [Product],
+
+  })
+  .then((categories) => res.json(categories))
+  .catch((err) => res.status(500).json(err));
+
+});
+
+router.get('/:id', (req, res) => {
+  Category.findOn({
+    where: {
+      id: req.params.id,
+    },
+    include: [Product],
+  })
+  .then((categories) => res.json(categories))
+  .catch((err) => res.status(500).json(err));
+
+});
+
+router.post('/', (req, res) => {
+  // create a new category
+});
+
+router.put('/:id', (req, res) => {
+  // update a category by its `id` value
+});
+
+router.delete('/:id', (req, res) => {
+  // delete a category by its `id` value
+});
+
+module.exports = router;
