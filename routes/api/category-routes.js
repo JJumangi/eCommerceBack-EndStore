a4eaed64ebe6e0727c39cf7c75ff35ab247e30fc
Jon A <jon.jumangi@gmail.com>
2023-08-13 12:53:13 -0500

    new commit/category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -27,27 +27,14 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
-  Category.create(req.body)
-  .then((categories) => res.json(categories))
-  .catch((err) => res.status(500).json(err));
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
-  Category.update(req.body)
-  .then((categories) => res.json(categories))
-  .catch((err) => res.status(500).json(err));
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
-  Category.destroy({
-    where: {
-      id: req.params.id,
-},
-})
-.then((categories) => res.json(categories))
-.catch((err) => res.status(500).json(err));
 });
 
 module.exports = router;
